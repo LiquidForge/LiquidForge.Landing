0b71e5adf77d1d8fee2d534a4b16e062883945e7
jguyet <jguyet.weoohh@gmail.com>
2024-05-08 01:38:25 +0200

    change bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,7 @@ export default {
       },
       backgroundImage: {
         "footer-img": `url(img/demo/footer-bg.webp)`,
-        "intro-img": `url(img/demo/large-name.png)`,
+        "intro-img": `url(img/demo/large-name-full.png)`,
       },
       transitionTimingFunction: {
         smooth: "cubic-bezier(0.23, 0.65, 0.74, 1.09)",
